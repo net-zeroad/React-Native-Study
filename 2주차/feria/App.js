66346f7ced8e4e40c66d97dd9a5555c85d45d4a5
efforthye <efforthye@gmail.com>
2023-03-19 02:44:37 +0900

    하나하나의 Card를 map 돌려 출력하기

--- 2주차/feria/App.js
+++ 2주차/feria/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
-
 // 큰 이미지 : https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fmain.png?alt=media&token=8e5eb78d-19ee-4359-9209-347d125b322c
 // 작은 피자 이미지 : https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fpizza.png?alt=media&token=1a099927-d818-45d4-b48a-7906fd0d2ad3
 // 꿀팁 제목: 먹다 남은 피자를 촉촉하게!
@@ -9,11 +8,19 @@ import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'rea
 // 가운데 버튼 4가지 : 생활, 재테크, 반려견, 꿀팁 찜
 // 스크롤 : 횡 스크롤(row or column)
 // 버튼 : TouchableOpacity
+import data from './data.json';
 
 export default function App() {
 
   // 메인 이미지의 절대경로 주소를 가져온다.
   const main = "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fmain.png?alt=media&token=8e5eb78d-19ee-4359-9209-347d125b322c";
+
+  // data.json의 tip을 불러온다.
+  let tip = data.tip;
+  // 앱 상단에 띄울 오늘의 온도를 지정해준다. 
+  let todayWeather = 10 + 7;
+  // 앱 상단에 띄울 오늘의 날씨를 지정해준다. 
+  let todayCondition = "흐림"
 
   return (
     // View를 ScrollView로 만들어 준다.
@@ -30,55 +37,37 @@ export default function App() {
       </ScrollView>
       <View style={styles.cardContainer}>
         {/* 하나의 카드 영역을 나타내는 View */}
-        <View style={styles.card}>
+        {/* numberOfLines : 몇 줄까지 표시할 것인지 설정한다. */}
+        {/* <View style={styles.card}>
           <Image style={styles.cardImage} source={{ uri: "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fpizza.png?alt=media&token=1a099927-d818-45d4-b48a-7906fd0d2ad3" }} />
           <View style={styles.cardText}>
             <Text style={styles.cardTitle}>먹다 남은 피자를 촉촉하게 #1</Text>
-            {/* numberOfLines : 몇 줄까지 표시할 것인지 설정한다. */}
             <Text style={styles.cardDesc} numberOfLines={3}>먹다 남은 피자는 수분이 나비처럼 훨훨 날아가기 때문에 처음처럼 맛있게 먹을 수 없는데요. 이럴 경우 그릇에 물을 받아 전자레인지 안에서 1분 30초에서 2분 정도 함께 돌려주면 촉촉하게 먹을 수 있습니다. 물이 전자레인지 안에서 수증기를 일으키고, 피자에 촉촉함을 더해줍니다.</Text>
             <Text style={styles.cardDate}>2023. 3. 19.</Text>
           </View>
-        </View>
-        {/* 하나의 카드 영역을 나타내는 View */}
-        <View style={styles.card}>
-          <Image style={styles.cardImage} source={{ uri: "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fpizza.png?alt=media&token=1a099927-d818-45d4-b48a-7906fd0d2ad3" }} />
-          <View style={styles.cardText}>
-            <Text style={styles.cardTitle}>먹다 남은 피자를 촉촉하게 #2</Text>
-            {/* numberOfLines : 몇 줄까지 표시할 것인지 설정한다. */}
-            <Text style={styles.cardDesc} numberOfLines={3}>먹다 남은 피자는 수분이 나비처럼 훨훨 날아가기 때문에 처음처럼 맛있게 먹을 수 없는데요. 이럴 경우 그릇에 물을 받아 전자레인지 안에서 1분 30초에서 2분 정도 함께 돌려주면 촉촉하게 먹을 수 있습니다. 물이 전자레인지 안에서 수증기를 일으키고, 피자에 촉촉함을 더해줍니다.</Text>
-            <Text style={styles.cardDate}>2023. 3. 19.</Text>
+        </View> */}
+        {/* 
+            "idx": 0,
+            "category": "생활",
+            "title": "먹다 남은 피자를 촉촉하게!",
+            "image": "https://storage.googleapis.com/sparta-image.appspot.com/lecture/pizza.png",
+            "desc": "먹다 남은 피자는 수분이 날라가기 때문에 처음처럼 맛있게 먹을 수 없는데요. 이럴 경우 그릇에 물을 받아 전자레인지 안에서 1분 30초에서 2분 정도 함께 돌려주면 촉촉하게 먹을 수 있습니다. 물이 전자레인지 안에서 수증기를 일으키고, 피자에 촉촉함을 더해줍니다.",
+            "date": "2020.09.09"
+        */}
+
+
+        {tip.map((item, idx) => (
+          <View style={styles.card} key={idx}>
+            <Image style={styles.cardImage} source={{ uri: item.image }} />
+            <View style={styles.cardText}>
+              <Text style={styles.cardTitle} numberOfLines={1}>
+                #{idx + 1} {item.title}
+              </Text>
+              <Text style={styles.cardDesc} numberOfLines={3}>{item.desc}</Text>
+              <Text style={styles.cardDate}>{item.date}</Text>
+            </View>
           </View>
-        </View>
-        {/* 하나의 카드 영역을 나타내는 View */}
-        <View style={styles.card}>
-          <Image style={styles.cardImage} source={{ uri: "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fpizza.png?alt=media&token=1a099927-d818-45d4-b48a-7906fd0d2ad3" }} />
-          <View style={styles.cardText}>
-            <Text style={styles.cardTitle}>먹다 남은 피자를 촉촉하게 #3</Text>
-            {/* numberOfLines : 몇 줄까지 표시할 것인지 설정한다. */}
-            <Text style={styles.cardDesc} numberOfLines={3}>먹다 남은 피자는 수분이 나비처럼 훨훨 날아가기 때문에 처음처럼 맛있게 먹을 수 없는데요. 이럴 경우 그릇에 물을 받아 전자레인지 안에서 1분 30초에서 2분 정도 함께 돌려주면 촉촉하게 먹을 수 있습니다. 물이 전자레인지 안에서 수증기를 일으키고, 피자에 촉촉함을 더해줍니다.</Text>
-            <Text style={styles.cardDate}>2023. 3. 19.</Text>
-          </View>
-        </View>
-        {/* 하나의 카드 영역을 나타내는 View */}
-        <View style={styles.card}>
-          <Image style={styles.cardImage} source={{ uri: "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fpizza.png?alt=media&token=1a099927-d818-45d4-b48a-7906fd0d2ad3" }} />
-          <View style={styles.cardText}>
-            <Text style={styles.cardTitle}>먹다 남은 피자를 촉촉하게 #4</Text>
-            {/* numberOfLines : 몇 줄까지 표시할 것인지 설정한다. */}
-            <Text style={styles.cardDesc} numberOfLines={3}>먹다 남은 피자는 수분이 나비처럼 훨훨 날아가기 때문에 처음처럼 맛있게 먹을 수 없는데요. 이럴 경우 그릇에 물을 받아 전자레인지 안에서 1분 30초에서 2분 정도 함께 돌려주면 촉촉하게 먹을 수 있습니다. 물이 전자레인지 안에서 수증기를 일으키고, 피자에 촉촉함을 더해줍니다.</Text>
-            <Text style={styles.cardDate}>2023. 3. 19.</Text>
-          </View>
-        </View>
-        {/* 하나의 카드 영역을 나타내는 View */}
-        <View style={styles.card}>
-          <Image style={styles.cardImage} source={{ uri: "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fpizza.png?alt=media&token=1a099927-d818-45d4-b48a-7906fd0d2ad3" }} />
-          <View style={styles.cardText}>
-            <Text style={styles.cardTitle}>먹다 남은 피자를 촉촉하게 #5</Text>
-            {/* numberOfLines : 몇 줄까지 표시할 것인지 설정한다. */}
-            <Text style={styles.cardDesc} numberOfLines={3}>먹다 남은 피자는 수분이 나비처럼 훨훨 날아가기 때문에 처음처럼 맛있게 먹을 수 없는데요. 이럴 경우 그릇에 물을 받아 전자레인지 안에서 1분 30초에서 2분 정도 함께 돌려주면 촉촉하게 먹을 수 있습니다. 물이 전자레인지 안에서 수증기를 일으키고, 피자에 촉촉함을 더해줍니다.</Text>
-            <Text style={styles.cardDate}>2023. 3. 19.</Text>
-          </View>
-        </View>
+        ))}
 
       </View>
 
