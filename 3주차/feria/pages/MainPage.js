511c6f7d6eb4b2673ad309f2af81568a866e13c7
efforthye <efforthye@gmail.com>
2023-03-19 06:36:42 +0900

    MainPage Category 완성

--- 3주차/feria/pages/MainPage.js
+++ 3주차/feria/pages/MainPage.js
@@ -13,30 +13,50 @@ export default function MainPage() {
     // useState() 를 사용해 본다.
     const [state, setState] = useState([]);
     const [ready, setReady] = useState(true);
+    const [category, setCategory] = useState([]);
 
     // useEffect() 를 사용해 본다.
     useEffect(() => {
 
         setTimeout(() => {
-            setState(data);
+            setState(data.tip);
+            setCategory(data.tip);
             setReady(false);
         }, 1000);
 
     }, []);
 
-    // 이처럼 화면이 없을 때 로딩 화면을 우선 보여주면 된다.
+    // 카테고리 설정 함수
+    const categoryFunc = (cate) => {
+        // setCategory(cate);
+
+        if (cate == "전체보기") {
+            // 기존 state 값을 category로 설정한다.
+            setCategory(state);
+        } else {
+            // state : 전체 상태 데이터가 들어있다.
+            // d : state 안에 있는 값, d.category : 그 안의 category를 사용한다.
+            // filter 함수를 통하여 새로운 리스트가 만들어지게 된다.
+            setCategory(state.filter((d) => {
+                // 매개변수로 받은 카테고리를 현재 상태(state)의 카테고리로 변경한다.
+                return d.category == cate;
+            }));
+        }
+    }
+
 
 
     // data.json의 tip을 불러온다.
-    let tipCards = state.tip;
+    let tipCards = state;
     // 앱 상단에 띄울 오늘의 온도를 지정해준다. 
     let todayWeather = 10 + 7;
     // 앱 상단에 띄울 오늘의 날씨를 지정해준다. 
     let todayCondition = "흐림"
 
     return (
-        // View를 ScrollView로 만들어 준다.
+        // 이처럼 화면이 없을 때 로딩 화면을 우선 보여주면 된다.
         ready ? <Loading /> : (
+            // View를 ScrollView로 만들어 준다.
             <ScrollView style={styles.container}>
 
                 <Text style={styles.title}>혜리미에 꿀팁 ㅇㅅㅇ</Text>
@@ -47,15 +67,32 @@ export default function MainPage() {
                 {/* 카테고리 목록, horizontal : 아이템들을 펼쳐지게 한다(중요), indicatorStyle : 무슨 역할인지 모르겠다. */}
                 {/* ScrollView 가로 정렬(수평 스크롤, 횡 스크롤) */}
                 <ScrollView style={styles.middleContainer} horizontal indicatorStyle={"white"}>
-                    <TouchableOpacity style={styles.middleButton01}><Text style={styles.middleButtonText}>생활</Text></TouchableOpacity>
-                    <TouchableOpacity style={styles.middleButton02}><Text style={styles.middleButtonText}>재테크</Text></TouchableOpacity>
-                    <TouchableOpacity style={styles.middleButton03}><Text style={styles.middleButtonText}>반려견</Text></TouchableOpacity>
-                    <TouchableOpacity style={styles.middleButton04}><Text style={styles.middleButtonText}>찜한 꿀팁</Text></TouchableOpacity>
+                    <TouchableOpacity style={styles.middleButtonAll} onPress={() => {
+                        categoryFunc('전체보기')
+                    }}><Text style={styles.middleButtonText}>전체보기</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.middleButton01} onPress={() => {
+                        categoryFunc('생활')
+                    }}><Text style={styles.middleButtonText}>생활</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.middleButton02} onPress={() => {
+                        categoryFunc('재테크')
+                    }}><Text style={styles.middleButtonText}>재테크</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.middleButton03} onPress={() => {
+                        categoryFunc('반려견')
+                    }}><Text style={styles.middleButtonText}>반려견</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.middleButton04} onPress={() => {
+                        categoryFunc('찜한 꿀팁')
+                    }}><Text style={styles.middleButtonText}>찜한 꿀팁</Text>
+                    </TouchableOpacity>
                 </ScrollView>
 
                 <View style={styles.cardContainer}>
 
-                    {tipCards.map((item, idx) => (
+                    {/* {tipCards.map((item, idx) => ( */}
+                    {category.map((item, idx) => (
                         <Card item={item} idx={idx} key={idx} />
                     ))}
 
@@ -113,6 +150,16 @@ const styles = StyleSheet.create({
         // backgroundColor: 'lightgray'
     },
     // 첫 번째 버튼 설정
+    middleButtonAll: {
+        width: 100,
+        height: 50,
+        padding: 15,
+        backgroundColor: "#56ACA9",
+        borderColor: "#56ACA9",
+        borderRadius: 15,
+        margin: 7
+
+    },
     middleButton01: {
         width: 100,
         height: 50,
