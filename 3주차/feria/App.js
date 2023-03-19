21e49264a2ebabe3f22d0303d3dcc4a1d5625798
efforthye <efforthye@gmail.com>
2023-03-19 19:48:24 +0900

    네비게이션 설정 - NavigationContainer, createStackNavigator()/Stack, Stack.Navigator/screenOptions, Stack.Navigator/name/component

--- 3주차/feria/App.js
+++ 3주차/feria/App.js
@@ -1,15 +1,27 @@
 import React from 'react'
+// import MainPage from './pages/MainPage';
+// import AboutPage from './pages/AboutPage';
+// import DetailPage from './pages/DetailPage';
+import { StatusBar } from 'expo-status-bar';
 
-import MainPage from './pages/MainPage';
-import AboutPage from './pages/AboutPage';
-import DetailPage from './pages/DetailPage';
+// 메인에 세팅할 네비게이션 라이브러리
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigator from './navigation/StackNavigator'
 
 
 export default function App() {
 
+  console.disableYellowBox = true;
+
   return (
     <>
-      <MainPage />
+      {/* <MainPage /> */}
+      <NavigationContainer>
+        <StatusBar style="black" />
+        <StackNavigator />
+      </NavigationContainer>
     </>
   )
 }
+
+
